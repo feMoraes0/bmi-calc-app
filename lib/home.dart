a424148d9451f14c780d449b6c2992a8bf295166
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-28 22:51:17 +0100

    Added validation to form

--- lib/home.dart
+++ lib/home.dart
@@ -6,10 +6,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String infoText = "Add your data.";
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   void _resetFields() {
     weightController.text = "";
@@ -22,19 +23,19 @@ class _HomeState extends State<Home> {
   void _calculate() {
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text) / 100.0;
-    double bmi = weight / (height * height);
-
     String message;
 
-    if(bmi < 18.6) {
+    double bmi = weight / (height * height);
+
+    if (bmi < 18.6) {
       message = "Underweight ${bmi.toStringAsPrecision(3)}";
-    } else if(bmi >= 18.6 && bmi < 24.9) {
+    } else if (bmi >= 18.6 && bmi < 24.9) {
       message = "Ideal weight ${bmi.toStringAsPrecision(3)}";
-    } else if(bmi >= 24.9 && bmi < 29.9) {
+    } else if (bmi >= 24.9 && bmi < 29.9) {
       message = "Slighly overweight ${bmi.toStringAsPrecision(3)}";
-    } else if(bmi >= 29.9 && bmi < 34.9) {
+    } else if (bmi >= 29.9 && bmi < 34.9) {
       message = "Grade 1 obesity ${bmi.toStringAsPrecision(3)}";
-    } else if(bmi >= 34.9 && bmi < 39.9) {
+    } else if (bmi >= 34.9 && bmi < 39.9) {
       message = "Grade 2 obesity ${bmi.toStringAsPrecision(3)}";
     } else {
       message = "Grade 3 obesity ${bmi.toStringAsPrecision(3)}";
@@ -62,62 +63,79 @@ class _HomeState extends State<Home> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch, // to use all width
-          children: <Widget>[
-            Icon(
-              Icons.person_outline,
-              size: 120.0,
-              color: Colors.greenAccent,
-            ),
-            TextField(
-              controller: weightController,
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.greenAccent),
-              decoration: InputDecoration(
-                labelText: 'Weight (kg)',
-                labelStyle: TextStyle(
-                  color: Colors.greenAccent,
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, // to use all width
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                size: 120.0,
+                color: Colors.greenAccent,
               ),
-            ),
-            TextField(
-              controller: heightController,
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.greenAccent),
-              decoration: InputDecoration(
-                labelText: 'Height (cm)',
-                labelStyle: TextStyle(
-                  color: Colors.greenAccent,
-                ),
-              ),
-            ),
-            Container(
-              height: 50.0,
-              margin: const EdgeInsets.symmetric(vertical: 20.0),
-              child: RaisedButton(
-                onPressed: _calculate,
-                child: Text(
-                  "Calculate!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25.0,
+              TextFormField(
+                controller: weightController,
+                validator: (input) {
+                  if(input.isEmpty) {
+                    return "Add your weight";
+                  }
+                },
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.greenAccent),
+                decoration: InputDecoration(
+                  labelText: 'Weight (kg)',
+                  labelStyle: TextStyle(
+                    color: Colors.greenAccent,
                   ),
                 ),
-                color: Colors.greenAccent,
               ),
-            ),
-            Text(
-              infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.greenAccent,
-                fontSize: 25.0,
+              TextFormField(
+                controller: heightController,
+                validator: (input) {
+                  if(input.isEmpty) {
+                    return "Add your height";
+                  }
+                },
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.greenAccent),
+                decoration: InputDecoration(
+                  labelText: 'Height (cm)',
+                  labelStyle: TextStyle(
+                    color: Colors.greenAccent,
+                  ),
+                ),
               ),
-            ),
-          ],
+              Container(
+                height: 50.0,
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    if(_formKey.currentState.validate()) {
+                      _calculate();
+                    }
+                  },
+                  child: Text(
+                    "Calculate!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  color: Colors.greenAccent,
+                ),
+              ),
+              Text(
+                infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.greenAccent,
+                  fontSize: 25.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
